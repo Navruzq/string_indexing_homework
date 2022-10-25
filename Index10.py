4f1c94f49f66ca7fbd52feb0253730baccd8120a
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-10-25 16:54:09 +0300

    10

--- Index10.py
+++ Index10.py
@@ -6,4 +6,20 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    a=0
+    if type(int(s[0]))==int:
+        a+=int(s[0])
+
+    if type(int(s[1]))==int:
+        a+=int(s[1])
+
+    if type(int(s[2]))==int:
+        a+=int(s[2])
+
+    if type(int(s[3]))==int:
+        a+=int(s[3])
+
+    if type(int(s[4]))==int:
+        a+=int(s[4])
+    return a 
+print(main('26736'))
