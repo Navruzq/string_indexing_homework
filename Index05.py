c805d2e9e27fd8b411e3a80ce8aa91f291dd19c6
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-10-25 15:53:18 +0300

    5 v

--- Index05.py
+++ Index05.py
@@ -1,3 +1,11 @@
+from re import T
+
+
+def isDigit(x):
+    if x>='0' and x<='9':
+        return True
+    else:
+        return False
 def main(s):
     """
     Given a variable s string of length five. Determine the number of digits involved in this variable.
@@ -6,4 +14,16 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    k=0
+    if isDigit(s[0]):
+        k+=1
+    if isDigit(s[1]):
+        k+=1
+    if isDigit(s[2]):
+        k+=1
+    if isDigit(s[3]):
+        k+=1
+    if isDigit(s[4]):
+        k+=1
+    return k
+print(main('1djd4'))
